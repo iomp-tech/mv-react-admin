883445c6f7de3b6e51f76a4695a6dafc128c9275
Арсений Нагибин <58362217+nagibin88@users.noreply.github.com>
2021-02-17 14:35:08 +0300

    version 1.2

--- src/components/Footer/FooterContactEdit.jsx
+++ src/components/Footer/FooterContactEdit.jsx
@@ -22,38 +22,6 @@ const FooterContactEdit = (props) => {
         <Edit {...props}>
             <SimpleForm toolbar={<UserEditToolbar />}>
                 <TextInput
-                    label="Facebook"
-                    source="facebook"
-                    style={style}
-                    validate={[required()]}
-                />
-                <TextInput
-                    label="Instagram"
-                    source="inst"
-                    style={style}
-                    validate={[required()]}
-                    multiline
-                />
-                <TextInput
-                    label="VK"
-                    source="vk"
-                    style={style}
-                    validate={[required()]}
-                />
-                <TextInput
-                    label="YouTube"
-                    source="youtube"
-                    style={style}
-                    validate={[required()]}
-                    multiline
-                />
-                <TextInput
-                    label="Telegram"
-                    source="telegram"
-                    style={style}
-                    validate={[required()]}
-                />
-                <TextInput
                     label="Email"
                     source="email"
                     style={style}
